101602041c3b991c51b21d1e2c18f6dca11f6727
BangDSN <94975680+BangDSN@users.noreply.github.com>
2025-07-22 00:01:51 +0200

    Update slideshow.js

--- slideshow.js
+++ slideshow.js
@@ -1,22 +1,38 @@
-const images = [
-  "images/billede1.jpg",
-  "images/billede2.jpg",
-  "images/billede3.jpg",
-  "images/billede4.jpg",
-  "images/billede5.jpg",
-  "images/billede6.jpg",
-  "images/billede7.jpg",
-  "images/billede8.jpg",
-  "images/billede9.jpg",
-  "images/billede10.jpg",
-  "images/billede11.jpg",
-  "images/billede12.jpg"
+// Slideshow
+const imageList = [
+  "images/IMG_0602.jpg",
+  "images/IMG_0968.HEIC",
+  "images/IMG_1089.JPG",
+  "images/IMG_1178.JPG",
+  "images/IMG_1340.HEIC",
+  "images/IMG_1354.HEIC",
+  "images/IMG_4122.JPG",
+  "images/IMG_4135.JPG",
+  "images/IMG_4246.JPG",
+  "images/IMG_4253.JPG",
+  "images/IMG_4270.JPG",
+  "images/IMG_4293.JPG",
+  "images/IMG_4339.JPG",
+  "images/IMG_4431.JPG",
+  "images/IMG_4449.JPG",
+  "images/IMG_4614.JPG",
+  "images/IMG_4712.JPG",
+  "images/IMG_4731.JPG",
+  "images/IMG_4733.JPG",
+  "images/IMG_4737.JPG",
+  "images/IMG_4852.JPG"
 ];
 
-let index = 0;
-const imgElement = document.getElementById("slideshow");
+let currentIndex = 0;
+const slideshow = document.getElementById("slideshow");
 
-setInterval(() => {
-  index = (index + 1) % images.length;
-  imgElement.src = images[index];
-}, 7000); // Skift hvert 7. sekund
+function showNextImage() {
+  currentIndex = (currentIndex + 1) % imageList.length;
+  slideshow.style.opacity = 0;
+  setTimeout(() => {
+    slideshow.src = imageList[currentIndex];
+    slideshow.style.opacity = 1;
+  }, 500);
+}
+
+setInterval(showNextImage, 7000); // Skift hver 7. sekund
